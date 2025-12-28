2b1fb1d9a893c2ac144e86cfb1b6f481116965d3
Daniel Boechat <dboechatt01@gmail.com>
2025-12-27 21:25:43 -0400

    Initial commit

---
+++ CursoemVideo/Exercicios/Ex008.js
@@ -0,0 +1,7 @@
+// console.log('O console funcionou corretamente!')
+var vel = 90
+console.log( `A velocidade do seu carro é de ${vel}km/h`)
+if(vel > 80){
+    console.log('Você ultrapassou o limite de velocidade. MULTADO!')
+}
+    console.log('Dirija com cuidado. Use o cinto de segurança')
